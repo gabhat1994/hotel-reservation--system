28ee69fea9600e943e12b50d6d21ea6167cbd4c5
stamic1 <bhatgaurav96@gmail.com>
2020-07-22 20:39:57 +0530

    setup banner

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,15 @@
 import React from "react";
 import Hero from "../Components/Hero";
+import Banner from "../Components/Banner";
+import { Link } from "react-router-dom";
 export default function Error() {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title="404" subtitle="page not found ">
+        <Link to="/" className="btn-primary">
+          Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
--- frontend/src/pages/Rooms.js
+++ frontend/src/pages/Rooms.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Hero from "../Components/Hero";
-
+import Banner from "../Components/Banner";
+import { Link } from "react-router-dom";
 export default function Rooms() {
   return (
-    <div>
-      <Hero hero="roomsHero" />
-    </div>
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          Return Home
+        </Link>
+      </Banner>
+    </Hero>
   );
 }
