31adad39e8b47a117869d58ccd4c190df13f2abe
stamic1 <bhatgaurav96@gmail.com>
2020-07-22 21:06:21 +0530

    setting up services section using resusble component

---
+++ frontend/src/Components/Services.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+import Title from "./Title";
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "free cocktails",
+        info:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        icon: <FaHiking />,
+        title: "Endless Hikking",
+        info:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "Free Shutles",
+        info:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        icon: <FaBeer />,
+        title: "Strongest Beer",
+        info:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+    ],
+  };
+  render() {
+    return (
+      <section className="services">
+        <Title title="services"></Title>
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Hero from "../Components/Hero";
 import Banner from "../Components/Banner";
+import Services from "../Components/Services";
 import { Link } from "react-router-dom";
 export default function hello() {
   return (
-    <Hero>
-      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 }
