b9c2872db16f9ea3b27a45e30691c7acfa6348cc
stamic1 <bhatgaurav96@gmail.com>
2020-07-22 19:23:55 +0530

    react router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Rooms from "../src/pages/Rooms";
+import Error from "../src/pages/Error";
+import SingleRoom from "../src/pages/SingleRoom";
 
 function App() {
-  return <>hellow world</>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/rooms/" component={Rooms} exact />
+        <Route path="/rooms/:slug" component={SingleRoom} exact />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
